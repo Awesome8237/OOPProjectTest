390a151b7238fa2fdcca6146430ef39dd9c2bfc1
Awesome8237 <i230666@isb.nu.edu.pk>
2024-05-13 02:12:50 +0500

    First test

---
+++ CherryBomb.h
@@ -0,0 +1,67 @@
+//
+// Created by admin on 5/6/2024.
+//
+
+#ifndef SFMLTEST_CHERRYBOMB_H
+#define SFMLTEST_CHERRYBOMB_H
+
+#include <SFML/Graphics.hpp>
+#include "Plant.h"
+#include "Zombie.h"
+using namespace sf;
+using namespace std;
+
+class CherryBomb: public Plant{
+
+
+    Zombie** zombie;
+
+    Clock timer;
+public:
+
+    const Clock &getTimer() const {
+        return timer;
+    }
+
+    void setTimer(const Clock &timer) {
+        CherryBomb::timer = timer;
+    }
+
+    bool isExploded() const {
+        return exploded;
+    }
+
+    void setExploded(bool exploded) {
+        CherryBomb::exploded = exploded;
+    }
+
+private:
+
+    bool exploded;
+
+public:
+
+    CherryBomb(Zombie** zombies, int lane);
+
+    virtual void update();
+
+
+
+
+    virtual void idleAnimation(RenderWindow& window);
+
+
+
+
+
+};
+
+
+
+
+
+
+
+
+
+#endif //SFMLTEST_CHERRYBOMB_H
---
+++ Interface.h
@@ -0,0 +1,19 @@
+//
+// Created by admin on 5/11/2024.
+//
+
+#ifndef SFMLTEST_INTERFACE_H
+#define SFMLTEST_INTERFACE_H
+
+class Interface{
+
+
+
+
+
+
+
+
+};
+
+#endif //SFMLTEST_INTERFACE_H
